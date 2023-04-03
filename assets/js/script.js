80d5b96d7fb3a2e0889ded4829a120a77658bdac
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-04-03 18:02:10 -0400

    Modified HTML to add other boxes and JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+$(function() {
+  // TODO: Add a listener for click events on the save button.
+  $(".saveBtn").on("click", function() {
+    // Use the id in the containing time-block as a key to save the user input in local storage
+    var key = $(this).parent().attr("id");
+    var value = $(this).siblings(".description").val();
+    localStorage.setItem(key, value);
+  });
+
+  // TODO: Add code to apply the past, present, or future class to each time block
+  const currentHour = new Date().getHours(); // Get the current hour using Day.js
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]); // Get the hour from the id
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+
+  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements
+  $(".description").each(function() {
+    var key = $(this).parent().attr("id");
+    $(this).val(localStorage.getItem(key));
+  });
+
+  // TODO: Add code to display the current date in the header of the page
+  var currentDate = dayjs().format("dddd, MMMM D, YYYY");
+  $("#currentDay").text(currentDate);
+});
+
+
+
+
+
+
+
